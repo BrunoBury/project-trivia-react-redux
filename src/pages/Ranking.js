5feedd5e02c10e508feb4c82b860bcefe1e35c24
Arthur Silvani <arthur.silvani@gmail.com>
2023-06-26 17:11:24 -0300

    Feat: creates home button on ranking screen req 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,29 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">
-        Ranking
-      </h1>
+      <>
+        <h1 data-testid="ranking-title">
+          Ranking
+        </h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ () => history.push('./') }
+        >
+          Home
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
